f13876b19f52fd1a6456f2f393a4e25e8e6bae67
Indonode.dev <34649601+elangrr@users.noreply.github.com>
2022-09-03 08:29:34 +0700

    Create haqq.sh

---
+++ haqq/haqq.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+echo -e "\033[0;35m"
+echo " :::::::::    :::               :::::       ::::::       :::: ::::::::::::";
+echo " :+:          :+:              ::   ::       :+:+:       :+:  :+:      :+:";
+echo " +:+          +:+             :+      +:     +:+  ::     +:+  +:+      +:+";
+echo " :#++:::++    ++#            ++###++++###    ##+   +#    +#+  #+#         ";
+echo " +#+          +#+           +#           #+  +#+    #+   #+#  +#+  #+#+#++#";
+echo " #+#          #+#          #+             +# #+#      +# +#+  #+#      #+#";
+echo " #########    ##########  ##               #####       #####  ############";
+echo -e '\e[36mWebsite:\e[39m' https://indonode.dev/
+echo -e '\e[36mGithub:\e[39m'  https://github.com/elangrr
+echo -e "\e[0m"
+
+sleep 2 
+
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Enter node name: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export HAQQ_CHAIN_ID=haqq_54211-2" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo '================================================='
+echo -e "Your node name: \e[1m\e[32m$NODENAME\e[0m"
+echo -e "Your wallet name: \e[1m\e[32m$WALLET\e[0m"
+echo -e "Your chain name: \e[1m\e[32m$POINT_CHAIN_ID\e[0m"
+echo '================================================='
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
+
+# install go
+rm -r /usr/local/go
+rm -r /usr/lib/go-1.13
+wget https://golang.org/dl/go1.18.3.linux-amd64.tar.gz; \
+rm -rv /usr/local/go; \
+tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz && \
+rm -v go1.18.3.linux-amd64.tar.gz && \
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile && \
+source ~/.bash_profile && \
+go version
+
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download binary
+cd $HOME
+git clone https://github.com/haqq-network/haqq && cd haqq-network
+make install
+
+# init
+haqqd init $NODENAME --chain-id $HAQQ_CHAIN_ID
+
+#config
+haqqd config chain-id $HAQQ_CHAIN_ID
+
+
+echo '=============== SETUP FINISHED ==================='
