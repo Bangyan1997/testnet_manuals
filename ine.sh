bf417467ebd3e4b91a805a7d30117edfcf72b755
Indonode.dev <34649601+elangrr@users.noreply.github.com>
2022-09-17 08:03:48 +0700

    Create ine.sh

---
+++ ine.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo -e "\033[0;35m"
+echo " ________      _________                  _________       _________";
+echo " ____  _/____________  /________________________  /____   ______  /_";
+echo "  __  / __  __ \  __  /_  __ \_  __ \  __ \  __  /_  _ \  _  __  /_  _ \_ | / /";
+echo " __/ /  _  / / / /_/ / / /_/ /  / / / /_/ / /_/ / /  __/__/ /_/ / /  __/_ |/ /";
+echo " /___/  /_/ /_/\__,_/  \____//_/ /_/\____/\__,_/  \___/_(_)__,_/  \___/_____/ ";
+echo "______________________________________________________________________________";
+echo " ==============================================================================";
+echo -e '\e[36mWebsite:\e[39m' https://indonode.dev/
+echo -e '\e[36mGithub:\e[39m'  https://github.com/elangrr
+echo -e "\e[0m"
+
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt-get install -y make bzip2 automake libbz2-dev libssl-dev doxygen graphviz libgmp3-dev autotools-dev libicu-dev python2.7 python2.7-dev python3 python3-dev autoconf libtool curl zlib1g-dev sudo ruby libusb-1.0-0-dev libcurl4-gnutls-dev pkg-config patch llvm-7-dev clang-7 vim-common jq libncurses5
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download binary
+cd $HOME
+git clone  https://github.com/inery-blockchain/inery-node \
+cd inery-node && \
+inery inery.setup
+
+#config
+cd inery.setup \
+chmod +x ine.py \
+./ine.py --export \
+cd; source .bashrc; cd -
+
+echo '=============== SETUP FINISHED ==================='
+echo -e 'To check logs: \e[1m\e[32m finished \e[0m'
