b3ded944de1d47a77e711db0e7be5a7656d0c65f
Indonode.dev <34649601+elangrr@users.noreply.github.com>
2022-09-17 08:22:34 +0700

    Update ine.sh

--- inery/ine.sh
+++ inery/ine.sh
@@ -21,18 +21,13 @@ echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
 # packages
 sudo apt-get install -y make bzip2 automake libbz2-dev libssl-dev doxygen graphviz libgmp3-dev autotools-dev libicu-dev python2.7 python2.7-dev python3 python3-dev autoconf libtool curl zlib1g-dev sudo ruby libusb-1.0-0-dev libcurl4-gnutls-dev pkg-config patch llvm-7-dev clang-7 vim-common jq libncurses5
 
-sleep 2
-
 echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 # download binary
 cd $HOME
 git clone https://github.com/inery-blockchain/inery-node 
-cd inery-node
-
-#config
-cd inery.setup \
-chmod +x ine.py \
-./ine.py --export \
+cd inery-node/inery.setup/
+chmod +x ine.py
+./ine.py --export
 cd; source .bashrc; cd -
 
 echo '=============== SETUP FINISHED ==================='
