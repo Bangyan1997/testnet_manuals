82a1d50b442fb436f7a0a000e85c05dfef98af19
Indonode.dev <34649601+elangrr@users.noreply.github.com>
2022-09-17 08:17:57 +0700

    Update ine.sh

--- inery/ine.sh
+++ inery/ine.sh
@@ -25,7 +25,7 @@ sleep 2
 
 echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 # download binary
-cd $HOME
+cd $HOME \
 git clone  https://github.com/inery-blockchain/inery-node \
 cd inery-node &&
 
